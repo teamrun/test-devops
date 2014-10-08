1335d0372bda5ca1f40c89a09c434908c5a30611
teamrun <chenllos@163.com>
2014-10-08 16:43:40 +0800

    gulp task rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
 var gulp = require('gulp');
 
-gulp.task('default', []);
+var rename = require('gulp-rename');
+
+gulp.task('rename', function(){
+    gulp.src('app.js')
+        .pipe(rename( Date.now() + '.js' ))
+        .pipe(gulp.dest('./'));
+});
+
+
+gulp.task('default', ['rename']);
